e879a4f08e8018bd288eb8f49561ab25a5ee99e1
JacktheGEAT <jacklynch706@gmail.com>
2026-05-04 17:27:38 -0400

    react stuff works

--- main/src/App.jsx
+++ main/src/App.jsx
@@ -1,11 +1,39 @@
 
+import React from 'react';
 import './App.css';
 import Navbar from './navbar';
 
 function App() {
+  const bubble = () => {
+    return <>
+      <div className="bubble-cage">
+          <div className='bubble'></div>
+      </div>
+    </>
+  }
+
   return (
-    <div className="App">
-    </div>
+    <>
+      <Navbar />
+      <div className="App">
+        <div className="header">
+            <h1>Hello World</h1>
+            <p>My name is Jack Lynch.</p>
+            <p>I am a Computer Science Major and Mathematics Minor at Union College in Schenectady NY.</p>
+            <p>Here you can see me experimenting with HTML, CSS, JavaScript, and React</p>
+            <p>You can look at my github and various projec using the navbar up top.</p>
+        </div>
+        
+        <details>
+            <summary>Project Posters</summary>
+            <div>
+                <div style={{padding: 1+"%"}}></div>
+                
+            </div>
+        </details>
+      </div>
+      {bubble()}
+    </>
   );
 }
 
